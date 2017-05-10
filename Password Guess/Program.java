f6afa326b5080e5da0bb80a090145ea61684a989
ZanderKirilov <biross11@gmail.com>
2017-05-10 16:50:32 +0300

    Update Program.java

--- Password Guess/Program.java
+++ Password Guess/Program.java
@@ -1,1 +1,19 @@
+import java.util.Scanner;
+
+public class PasswordGuess {
+
+	@SuppressWarnings({ "unused", "resource" })
+	public static void main(String[] args) {
+		Scanner console = new Scanner(System.in);
+		String random = console.nextLine();
+		
+		if (random.equals("s3cr3t!P@ssw0rd")) {
+			System.out.println("Welcome");
+		} else {
+			System.out.println("Wrong password!");
+		}
+			
+	
+	} 
+}
 
